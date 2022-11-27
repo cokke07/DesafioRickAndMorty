557ac98f1ef8ddc2695b2490d3b6a1740192126f
unknown <coke.jgomez@gmail.com>
2022-11-27 13:34:06 -0300

    Comenzando con los archivos base del proyecto

---
+++ src/main/java/cl/cokke/DesafioRickAndMortyApplication.java
@@ -0,0 +1,13 @@
+package cl.cokke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRickAndMortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRickAndMortyApplication.class, args);
+	}
+
+}
