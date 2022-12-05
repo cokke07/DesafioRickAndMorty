4958c265d291813866fc0d2fb1a243010d3f86a1
unknown <coke.jgomez@gmail.com>
2022-12-05 12:07:50 -0300

    Se agrega servicio, repositorio, implementacion de User, archvos de seguridad

---
+++ src/main/java/cl/cokke/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package cl.cokke.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class WebSecurityConfig {
+
+	
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests()
+		.antMatchers("/swagger-ui/**").permitAll()
+		.antMatchers("/docs/**").permitAll()
+		.antMatchers("/auth/**").permitAll()
+		.anyRequest().authenticated();
+		//.antMatchers("/api/v1/disney/**").permitAll()
+		
+
+		http.exceptionHandling().accessDeniedPage("/login");
+		http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+
+		return http.build();
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12);
+	}
+
+	
+}
---
+++ src/main/java/cl/cokke/service/UserServiceImpl.java
@@ -0,0 +1,100 @@
+package cl.cokke.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cl.cokke.exception.RestServiceException;
+import cl.cokke.model.User;
+import cl.cokke.repository.UserRepository;
+import cl.cokke.security.JwtTokenProvider;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	//Inyeccion de nuevas utilidades de JWT para manejar autenticacion
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<User> findAll() {
+		// Recibe lista de User
+		return userRepository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public String login(String username, String password) {
+		try {
+			//Validar datos de inicio de sesion
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+			System.out.println(username);
+			System.out.println(password);
+			//Retorna token si los datos son correctos
+			return jwtTokenProvider.createToken(username, userRepository.findByUsername(username).getRoles());
+		} catch (AuthenticationException e) {
+			//Excepcion en caso de datos erroneos
+			throw new RestServiceException("username o password invalido", HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+	}
+
+	@Override
+	@Transactional
+	public User crearUser(User User) {
+		//Valida si el nombre de usuario no exista
+		if (!userRepository.existsByUsername(User.getUsername())) {
+			//Se encripta contraseña  
+			User.setPassword(passwordEncoder.encode(User.getPassword()));
+			//Se almacena el usuario
+			userRepository.save(User);
+			//Retrona token valido para este usuario
+			jwtTokenProvider.createToken(User.getUsername(), User.getRoles());
+			return User;
+		} else {
+			//En caso de que nombre de usuario exista se retonra excepcion
+			throw new RestServiceException("Username ya esta en uso", HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		//Se busca usuario por su username
+		final User user = userRepository.findByUsername(username);
+		//Se evalua si usuario existe
+		if (user == null) {
+			//Si no existe se retorna excepcion de "Usuario no encontrado"
+			throw new UsernameNotFoundException("Usuario '" + username + "' no encontrado");
+		}
+		//Si existe, se retorna un objeto de tipo UserDetails, validando contraseña y su respectivo Rol.
+		return org.springframework.security.core.userdetails.User
+				.withUsername(username)
+				.password(user.getPassword())
+				.authorities(user.getRoles())
+				.accountExpired(false)
+				.accountLocked(false)
+				.credentialsExpired(false)
+				.disabled(false)
+				.build();
+	}
+}
