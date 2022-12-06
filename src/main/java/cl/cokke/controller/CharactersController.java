160de899ac34d9aceed1cb88e4e7d02ea82473a0
unknown <coke.jgomez@gmail.com>
2022-12-06 18:11:01 -0300

    Se comienza con la documentacion del API

--- src/main/java/cl/cokke/controller/CharactersController.java
+++ src/main/java/cl/cokke/controller/CharactersController.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cl.cokke.service.CharacterService;
 import cl.cokke.model.Character;
+import cl.cokke.service.CharacterService;
 
 @RestController
 @RequestMapping("/")
