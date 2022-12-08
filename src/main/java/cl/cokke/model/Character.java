d9a13db7e27ab138ecb3eb8ce9034e0e31c4fac7
unknown <coke.jgomez@gmail.com>
2022-12-08 00:10:49 -0300

    Se agrega el detalle de la documentacion de Swagger a todos los endpoint

--- src/main/java/cl/cokke/model/Character.java
+++ src/main/java/cl/cokke/model/Character.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreType
 public class Character {
 
 	Info info;
